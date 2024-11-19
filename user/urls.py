94ea432af02e6170ce9478956eeb1436040eabc1
NaahilKDJ <134393859+NaahilKDJ@users.noreply.github.com>
2024-11-19 10:35:58 +0100

    ajstement de l'url des users

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from .views import 
+from django.urls import path 
 from .views import LoginView, SignupView
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('user/<int:pk>', Profil.as_view(), name='user-view'),
+    # path('user/<int:pk>', Profil.as_view(), name='user-view'),
     # path('create/', .as_view(), name='facture-create'),
     # path('update/<int:pk>/', .as_view(), name='facture-update'),
     # path('delete/<int:pk>/', .as_view(), name='facture-delete'),
