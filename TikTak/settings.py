9614c46d0b80f14d841f657ec902acd6ac3af493
NaahilKDJ <134393859+NaahilKDJ@users.noreply.github.com>
2024-11-20 09:50:34 +0100

    ajout de la base de donnée sqLite

--- TikTak/settings.py
+++ TikTak/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'TikTak.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql_psycopg2",
+       "NAME": "tiktak",
+       "USER": "demo",
+       "PASSWORD": "azertyuiop",
+       "HOST": "localhost",
+       "PORT": 5432,
+       "OPTIONS": {
+           "connect_timeout": 10,
+       },
+   }
 }
 
 
